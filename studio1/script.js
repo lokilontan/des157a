a7242b1dbd40df32d80a62165c201c4f65a4fb69
Vlad PlugOFF <lokilontan@yandex.ru>
2021-10-25 13:43:52 -0700

    did some progress bar handling

--- studio1/script.js
+++ studio1/script.js
@@ -3,21 +3,105 @@
 
     console.log("Reading JS...");
 
-    const myForm = document.querySelector("#myform");
+    // ----------------FORM stuff starts here--------------------
 
-    const myArticle = document.querySelector("#madlib");
+    const myForm = document.querySelector("#myform");
 
     let errorText = "";
 
+    // keep track of the state of the progrss bar [1..6]
+    let currentEntry = 1;
+    progressBarAt(1);
 
-    myForm.addEventListener("submit", function (event) {
+    // keep the words entered for the final story
+    let wordTokens = [];
+
+    const nextBtn = document.querySelector("input[id=next]");
+    const backBtn = document.querySelector("input[id=back]");
+
+    nextBtn.addEventListener("click", function (event) {
         event.preventDefault();
+        const formData = document.querySelector("input[type=text]");
 
-        const formData = document.querySelectorAll("input[type=text]");
-
-        getData(formData);
+        if ( currentEntry < 6 ) {
+            currentEntry++;
+        }
+        progressBarAt(currentEntry);
 
     });
+
+    backBtn.addEventListener("click", function (event) {
+
+        event.preventDefault();
+
+        const formData = document.querySelector("input[type=text]");
+        console.log("back: " + formData.value);
+        if ( currentEntry > 1 ) {
+            currentEntry--;
+        }
+        progressBarAt(currentEntry);
+
+    });
+
+    /**
+     * Maintaince a correct view of the progress bar at a particular state
+     * @param { Number } at The current entry state
+     */
+    function progressBarAt(at) {
+
+        for (let i = 1; i < at; i++) {
+            let bar = document.getElementById(`progress-${i}`);
+            bar.className = "bar done";
+            bar.textContent = `✓`;
+            let connect = document.getElementById(`connect-${i}`);
+            if ((at - 1) == i) {
+                connect.className = "connect current";
+            } else {
+                connect.className = "connect done";
+            }
+        }
+
+        let bar = document.getElementById(`progress-${at}`);
+        bar.className = "bar current";
+        bar.textContent = `${at}`;
+        let connect = null;
+        if (at != 6) {
+            connect = document.getElementById(`connect-${at}`);
+            connect.className = "connect todo";
+        }
+
+        for (let i = at + 1; i <= 6; i++) {
+            if (i != 6) {
+                bar = document.getElementById(`progress-${i}`);
+                bar.className = "bar todo";
+                bar.textContent = `${i}`;
+                connect = document.getElementById(`connect-${i}`);
+                connect.className = "connect todo";
+            } else {
+                bar = document.getElementById(`progress-${i}`);
+                bar.className = "bar todo";
+            }
+        }
+    }
+
+    // myForm.addEventListener("submit", function (event) {
+    //     event.preventDefault();
+
+    //     const formData = document.querySelector("input[type=text]");
+
+    //     console.log(formData.value);
+    //     // getData(formData);
+
+    // });
+
+
+    // ----------------FORM stuff ends here--------------------
+
+    // ----------------MADLIB stuff starts here----------------
+
+    const myArticle = document.querySelector("#madlib");
+
+
 
     function makeMadlib(tokens) {
         myArticle.textContent = "Here are the words: " + tokens[0] + " " + tokens[1] + " " + tokens[2] + " " + tokens[3];
@@ -35,43 +119,48 @@
                 emptyFields++
             }
         }
-        if (emptyFields>0) {
+        if (emptyFields > 0) {
             myArticle.textContent = 'Please fill out the  fields';
-        } else { 
+        } else {
             makeMadlib(tokens);
         }
 
     }
-    const randSize = () =>  Math.floor(Math.random()+1.2);
-    const randCol = () => Math.floor(Math.random()*256);
 
+    // ----------------MADLIB stuff ends here--------------------
+
+
+    // ----------------LOGO stuff starts here--------------------
+    const randSize = () => Math.floor(Math.random() + 1.1);
+    const randCol = () => Math.floor(Math.random() * 256);
 
     // innitialize random values to the cubes in logo
-    for (let i = 1 ; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         let cube = document.getElementById(`cube-${i}`);
-        cube.style.top = `${Math.floor( Math.random() * (15 - 2) + 2 )}%`;
-        cube.style.left = `${Math.floor( Math.random() * (25 - 10) + 10 )}%`;
-        cube.style.backgroundColor = `rgb(${randCol()},${randCol()},${randCol()})`;
+        cube.style.top = `${Math.floor(Math.random() * (43 - 19) + 19)}%`;
+        cube.style.left = `${Math.floor(Math.random() * (52 - 35) + 35)}%`;
+        cube.style.backgroundColor = `rgba(${randCol()},${randCol()},${randCol()},0.8)`;
 
-        const newSize = Math.floor(Math.random() * (60 - 15) + 15);
+        const newSize = Math.floor(Math.random() * (50 - 15) + 15);
         cube.style.width = `${newSize}px`;
         cube.style.height = `${newSize}px`;
 
     }
 
-    function changeProperties(){
-        for (let i = 1 ; i <= 10; i++) {
+    // function to change background color and size to random values
+    // goes over each cube in the logo
+    function changeProperties() {
+        for (let i = 1; i <= 10; i++) {
             let cube = document.getElementById(`cube-${i}`);
             const newSize = randSize();
-            cube.style.backgroundColor = `rgb(${randCol()},${randCol()},${randCol()})`;
+            cube.style.backgroundColor = `rgba(${randCol()},${randCol()},${randCol()},0.8)`;
             cube.style.transform = `scale(${newSize}, ${newSize})`;
         }
     }
 
-    setInterval(changeProperties, 9000);
+    // calls the changeProperties funnction each 8 seconds
+    setInterval(changeProperties, 5000);
 
-   
-
-
+    // ----------------LOGO stuff ends here--------------------
 
 }());
