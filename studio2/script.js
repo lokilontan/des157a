a7da8fce1232b55e2165e1c25c70244539a08144
Vlad PlugOFF <lokilontan@yandex.ru>
2021-11-05 17:19:36 -0700

    added zooming and returning to the initial state

--- studio2/script.js
+++ studio2/script.js
@@ -3,6 +3,8 @@
 
     console.log("reading JS...");
 
+    // BUTTON
+
     const button3D = document.getElementById("btn-3d");
     let button3DStatus = "inactive";
     button3D.addEventListener("click", function(event) {
@@ -15,5 +17,34 @@
             button3DStatus = "inactive";
         }
     });
+
+    // BUTTON END
+
+    let mainImage = document.getElementById("main");
+
+    window.addEventListener("click", function(event) {
+        event.preventDefault();
+        if (event.target.id == "main") {
+            mainImage.className = `state-${event.target.id}`;
+            this.setTimeout(unhideSectors, 2000);
+        } else {
+        mainImage.className = `state-${event.target.id}`;
+        hideSectors();
+        }
+    });
+
+    function unhideSectors() {
+        const sectors = document.getElementsByClassName("sect");
+        for (let sector of sectors) {
+            sector.style.setProperty("visibility", "visible");
+        }
+    }
+
+    function hideSectors() {
+        const sectors = document.getElementsByClassName("sect");
+        for (let sector of sectors) {
+            sector.style.setProperty("visibility", "hidden");
+        }
+    }
 
 }());
