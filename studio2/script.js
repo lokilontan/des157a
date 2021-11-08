391a294236c0aa2d06f2e416d3ae10e259e07214
Vlad PlugOFF <lokilontan@yandex.ru>
2021-11-07 19:01:22 -0800

    fixed some bugs with the articles

--- studio2/script.js
+++ studio2/script.js
@@ -19,58 +19,32 @@
     });
     // PRELOADER END
 
-    // BUTTON
-    const button3D = document.getElementById("btn-3d");
-    let button3DStatus = "inactive";
-    button3D.addEventListener("click", function (event) {
-        event.preventDefault();
-        if (button3DStatus == "inactive") {
-            button3D.className = "btn active";
-            button3DStatus = "active";
-        } else {
-            button3D.className = "btn inactive";
-            button3DStatus = "inactive";
-        }
-    });
-
-    // BUTTON END
-
     let mainImage = document.getElementById("main");
     let fade = document.querySelector("figure div");
     var currentArticle = "";
 
-    console.log(fade);
-
     window.addEventListener("click", function (event) {
         event.preventDefault();
         if (event.target.id == "fade") {
-
             mainImage.className = `state-main`;
             fade.className = `fade nofade`;
-
             articleOut();
-
-            console.log(event.target.id);
             this.setTimeout(unhideSectors, 2000);
-        } else {
+        } else if (event.target.className == "sect") {
             mainImage.className = `state-${event.target.id}`;
             fade.className = `fade ${event.target.id}`;
-
             articleIn(event.target.id);
-
-            console.log(event.target.id);
-
             hideSectors();
         }
     });
 
-    function articleIn(articleSection){
+    function articleIn(articleSection) {
         currentArticle = `${articleSection}`
         let article = document.getElementById(`article-${currentArticle}`);
         article.className = `overlay showing enter-${articleSection}`;
     }
 
-    function articleOut(){
+    function articleOut() {
         if (currentArticle) {
             let article = document.getElementById(`article-${currentArticle}`);
             article.className = `overlay hidden exit-${currentArticle}`;
