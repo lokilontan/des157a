587f735325d0add53ca42c255f1ce96ac24c0dd7
Vlad PlugOFF <lokilontan@yandex.ru>
2021-11-07 16:43:03 -0800

    articles appear correctly

--- studio2/script.js
+++ studio2/script.js
@@ -37,23 +37,45 @@
 
     let mainImage = document.getElementById("main");
     let fade = document.querySelector("figure div");
+    var currentArticle = "";
+
     console.log(fade);
 
     window.addEventListener("click", function (event) {
         event.preventDefault();
         if (event.target.id == "fade") {
+
             mainImage.className = `state-main`;
             fade.className = `fade nofade`;
+
+            articleOut();
+
             console.log(event.target.id);
             this.setTimeout(unhideSectors, 2000);
         } else {
             mainImage.className = `state-${event.target.id}`;
             fade.className = `fade ${event.target.id}`;
+
+            articleIn(event.target.id);
+
             console.log(event.target.id);
 
             hideSectors();
         }
     });
+
+    function articleIn(articleSection){
+        currentArticle = `${articleSection}`
+        let article = document.getElementById(`article-${currentArticle}`);
+        article.className = `overlay showing enter-${articleSection}`;
+    }
+
+    function articleOut(){
+        if (currentArticle) {
+            let article = document.getElementById(`article-${currentArticle}`);
+            article.className = `overlay hidden exit-${currentArticle}`;
+        }
+    }
 
     function unhideSectors() {
         const sectors = document.getElementsByClassName("sect");
