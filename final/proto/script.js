c0c3d53156c60ae7b484ce87aed473c0bc945f82
Vlad PlugOFF <lokilontan@yandex.ru>
2021-11-24 15:22:58 -0800

    prototype 1 first major addition imolemented

--- final/proto/script.js
+++ final/proto/script.js
@@ -3,6 +3,8 @@
 
     console.log("reading JS...");
 
+    let mainImage = document.getElementById("main");
+
     // PRELOADER
     const preloader = document.getElementById('preloader');
     document.onreadystatechange = function () {
@@ -10,6 +12,8 @@
         if (state == 'complete') {
             document.getElementById('interactive');
             preloader.className = 'fadeout';
+            document.getElementById('dimmed').className = "showing";
+            startHeaderAnimation();
         }
     }
     // wait until the animation has completed
@@ -19,7 +23,39 @@
     });
     // PRELOADER END
 
-    let mainImage = document.getElementById("main");
+    // STARTING ANIMATION START
+    function startHeaderAnimation() {
+        let headerBackground= document.getElementById('dimmed')
+        const timeout = 3000;
+        const headers = ['STREET SCRAMBLER', 'BEAUTIFUL', 'FAST', 'RELIABLE'];
+        let headerContainer = document.getElementById('heading');
+        mainImage.className =`state-heading`;
+        for (const i in headers) {
+            // show
+            setTimeout(function () {
+                headerContainer.className = "showing";
+                headerContainer.textContent = `${headers[i]}`;
+                setTimeout(function() {
+                    headerContainer.className = "hidden"
+                }, timeout/2);
+            }, timeout * i);
+            //hide
+            setTimeout(function() {
+                headerBackground.className = "hidden";
+                mainImage.className = `state-main`;
+            }, (headers.length) * timeout);
+        }
+        headerBackground.addEventListener("click", function(){
+            headerBackground.className = "hidden";
+            headerContainer.className = "hidden"
+            mainImage.className = `state-main`;
+        });
+    }
+
+
+
+    // STARTING ANIMATION END
+
     let fade = document.querySelector("figure div");
     var currentArticle = "";
 
